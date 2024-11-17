8387ec6ff67a73dfd23f5134d43b3b5ebe9835d9
Mohammed Faraaz <mhdfrz637@gmail.com>
2024-11-16 18:44:28 -0600

    all the methods and spinner

--- Wad1/src/app/app.module.ts
+++ Wad1/src/app/app.module.ts
@@ -1,24 +1,45 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card'; // Add this for <mat-card>
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './navigation/header/header.component';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
 import { FooterComponent } from './navigation/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {CommonModule} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    HeaderComponent,
+    FooterComponent,
+    CreatePostComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MatToolbarModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatExpansionModule,
+    HttpClientModule,CommonModule, MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
